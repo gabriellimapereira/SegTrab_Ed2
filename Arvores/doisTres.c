91d2227689853a28de030650c5c235b5596f1c72
gabriellimapereira <gaab.ollim@gmail.com>
2025-05-08 13:42:51 -0300

    parcial da Dois Trẽs

---
+++ Arvores/doisTres.c
@@ -0,0 +1,104 @@
+#pragma once
+#include <stdlib.h>
+
+// CÓDIGO DO DIABO
+
+typedef struct DoisTres {
+    int infoUm, infoDois, quantInfo;
+    DoisTres *esq, *cen, *dir;
+} DoisTres;
+
+DoisTres* criaNo(int info, DoisTres *fEsq, DoisTres *fCen)
+{
+    DoisTres *no;
+    no = (DoisTres*) malloc(sizeof(DoisTres));
+
+    if (no != NULL)
+    {
+        no->infoUm = info;
+        no->esq = fEsq;
+        no->cen = fCen;
+        no->dir = NULL;
+        no->quantInfo = 1;
+    }
+
+    return no;
+}
+
+void adicionaInfo(DoisTres **no, int info, DoisTres *subArvInfo)
+{
+    if (info > (*no)->infoUm)
+    {
+        (*no)->infoDois = info;
+        (*no)->dir = subArvInfo;
+    }
+    else 
+    {
+        (*no)->infoDois = (*no)->infoUm;
+        (*no)->infoUm = info;
+        (*no)->dir = (*no)->cen;
+        (*no)->cen = subArvInfo;
+    }
+
+    (*no)->quantInfo = 2;
+}
+
+DoisTres* quebraNo(DoisTres **no, int info, int *sobe, DoisTres *filhoDir)
+{
+    DoisTres *maior;
+
+    if (info > (*no)->infoDois)
+    {
+        *sobe = (*no)->infoDois;
+        maior = criaNo(info, (*no)->dir, filhoDir);
+        (*no)->quantInfo = 1;
+    }
+    else if (info > (*no)->infoUm) 
+    {
+        *sobe = info;
+        maior = criaNo((*no)->infoDois, filhoDir, (*no)->dir);
+        (*no)->quantInfo = 1;
+    }
+    else 
+    {
+        *sobe = (*no)->infoUm;
+        maior = criaNo((*no)->infoDois, (*no)->cen, (*no)->dir);
+        (*no)->infoUm = info;
+        (*no)->cen = filhoDir;
+        (*no)->quantInfo = 1;
+    }
+
+    return maior;
+}
+
+DoisTres* insere(DoisTres **raiz, DoisTres *pai, int info, int *sobe) 
+{
+    if (*raiz == NULL)
+    {
+        *raiz = criaNo(info, NULL, NULL);
+    }
+    else 
+    {
+        DoisTres *maiorNo;
+        if (ehFolha(*raiz))
+        {
+            if ((**raiz).quantInfo == 1)
+            {
+                adicionaInfo(raiz, info, NULL);
+            }
+            else 
+            {
+                maiorNo = quebraNo(raiz, info, sobe);
+                if (pai == NULL)
+                {
+                    *raiz = criaNo(*sobe, *raiz, maiorNo);
+                    maiorNo = NULL;
+                }
+            }
+        }
+        else  
+        {
+
+        }
+    }
+}
