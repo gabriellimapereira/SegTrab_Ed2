d010fffd2f53a3b8525c90b859de22c353c2d684
gabriellimapereira <gaab.ollim@gmail.com>
2025-05-20 13:29:32 -0300

    criação de algumas questões

--- QuestaoUm/prototiposUm.h
+++ QuestaoUm/prototiposUm.h
@@ -86,22 +86,72 @@ Estado* estadoMaisPopuloso(Estado *raiz);
 
 //Questões
 /*
-void populacaoDaCapital(Cidade *raiz, int capital, int populacao) 
+//questao 2: Qual a população da Capital de um determinado estado? 
+void populacaoDaCapital(Cidade *raiz, int capital, int populacao) //ARRUMAR A RAIZ PARA O UNION
 {
     if (raiz)
     {
-        if (raiz->info.nome == capital) if (raiz->info.populacao > populacao) populacao = raiz->info.populacao;
+        if(raiz->info.nome == capital) 
+        {
+            if(raiz->info.populacao > populacao)
+            {
+                populacao = raiz->info.populacao;
+            }
+        }
         populacaoDaCapital(raiz->esq, capital, populacao);
         populacaoDaCapital(raiz->dir, capital, populacao);
     }
 }
 
-void cidadeMaisPopulosa(Cidade *raiz, int capital, Cidade *cidade) 
+//questao 3: Qual a cidade mais populosa de um estado sem ser a Capital? 
+void cidadeMaisPopulosa(Cidade *raiz, int capital, Cidade *cidade) //ARRUMAR A RAIZ PARA O UNION
 {
     if (raiz)
     {
-        if (raiz->info.nome != capital) if (raiz->info.populacao > cidade->info.populacao) cidade = raiz;
-        populacaoDaCapital(raiz->esq, capital, cidade);
-        populacaoDaCapital(raiz->dir, capital, cidade);
+        if (raiz->info.nome != capital)
+        {
+            if (raiz->info.populacao > cidade->info.populacao) 
+            {
+                cidade = raiz;
+            }
+        }
+        cidadeMaisPopulosa(raiz->esq, capital, cidade);
+        cidadeMaisPopulosa(raiz->dir, capital, cidade);
     }
-} */
+} 
+
+//questao 4: Quantas pessoas não moram na cidade natal. 
+void quantidadePessoasCepAtual(ArvRubNeg *pessoas, int *quantidade)
+{
+    if(pessoas)
+    {
+        if(pessoas->info.pessoa->info.cepNatal != pessoas->info.pessoa->info.cepAtual)
+        {
+            (*quantidade)++;
+        }
+        quantidadePessoasCepAtual(pessoas->esq, quantidade);
+        quantidadePessoasCepAtual(pessoas->dir, quantidade);
+    }
+}
+
+questao 5: Qual cidade natal de uma pessoa dado o CEP da cidade? 
+
+questao 6: Quantas pessoas nascidas em uma determinada cidade não mora na cidade natal? 
+void quantidadePessoasCepNatal(ArvRubNeg *pessoas, int *quantidade, ArvRubNeg *cidade)
+{
+    int existe;
+    if(pessoas)
+    {
+        existe = verificaCep(cidade->info.ceps, pessoas->info.pessoa->cepNatal);
+        if(existe)
+        {
+            if(pessoas->info.pessoa->cepNatal != pessoas->info.pessoa->cepAtual)
+            {
+                (*quantidade)++;
+            }
+        }
+        quantidadePessoasCepNatal(pessoas->esq, quantidade, cidade);
+        quantidadePessoasCepNatal(pessoas->dir, quantidade, cidade);
+    }
+}
+*/
